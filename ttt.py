279a4f08085c2fdc2e45e6c1edc3b72f77d1c66f
taigamatumoto37 <taiga36tkg@gmail.com>
2026-01-20 22:44:58 +0900

    Update ttt.py

--- ttt.py
+++ ttt.py
@@ -71,12 +71,12 @@ def get_innate_deck():
 # --- 初期化 ---
 if 'deck' not in st.session_state or st.sidebar.button("♻️ ゲームをリセット"):
     common_deck = []
-    for _ in range(15): common_deck.append(Card("追撃・小剣", "attack", 25, "check_pair"))
+    for _ in range(13): common_deck.append(Card("追撃・小剣", "attack", 25, "check_pair"))
     for _ in range(10): common_deck.append(Card("強襲・大剣", "attack", 65, "check_three"))
-    for _ in range(6):  common_deck.append(Card("アイアン・ガード", "guard", 45, "check_pair"))
-    for _ in range(5):  common_deck.append(Card("癒しのハーブ", "heal", 35, "check_pair"))
-    for _ in range(3):  common_deck.append(Card("癒しの香水", "status", 15, "check_pair", effect="regen", duration=3))
-    for _ in range(3):  common_deck.append(Card("神聖な水", "status", 35, "check_pair", effect="regen", duration=2))
+    for _ in range(9):  common_deck.append(Card("アイアン・ガード", "guard", 45, "check_pair"))
+    for _ in range(9):  common_deck.append(Card("癒しのハーブ", "heal", 35, "check_pair"))
+    for _ in range(8):  common_deck.append(Card("癒しの香水", "status", 15, "check_pair", effect="regen", duration=3))
+    for _ in range(6):  common_deck.append(Card("神聖な水", "status", 35, "check_pair", effect="regen", duration=2))
     for _ in range(4):  common_deck.append(Card("猛毒の粉末", "status", 12, "check_pair", effect="poison", duration=3))
     for _ in range(4):  common_deck.append(Card("破滅の呪い", "status", 5, "check_three", effect="poison", duration=10))
     for _ in range(4):  common_deck.append(Card("死神の鎌", "status", 30, "check_full_house", effect="execute",duration=2)) # 追い打ち
@@ -314,4 +314,5 @@ elif st.session_state.phase == "counter":
             add_log("🔥", "覚醒！固有復活")
 
         switch_player(); st.rerun()
+
 
